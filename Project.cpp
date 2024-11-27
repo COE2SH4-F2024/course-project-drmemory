0ab24f8a1c31a081df68c5f3432b2deec28f26e8
Maryam Ahmed <ahmem121@mcmaster.ca>
2024-11-27 11:47:06 -0500

    1b

--- Project.cpp
+++ Project.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"
 
 using namespace std;
 
 #define DELAY_CONST 100000
+
+GameMechs* gameMech = nullptr;  
+
 
 bool exitFlag;
 
@@ -21,6 +25,8 @@ int main(void)
 {
 
     Initialize();
+    MacUILib_clearScreen();
+
 
     while(exitFlag == false)  
     {
@@ -39,34 +45,74 @@ void Initialize(void)
 {
     MacUILib_init();
     MacUILib_clearScreen();
+    gameMech = new GameMechs(30,15);
 
     exitFlag = false;
 }
 
 void GetInput(void)
-{
+{   char input = MacUILib_getChar();  
+    gameMech->setInput(input);
+
+    if (input == '!') {
+        gameMech->setExitTrue();
+    }
+
    
 }
 
-void RunLogic(void)
-{
+void RunLogic(void){
+    if (gameMech->getExitFlagStatus())
+    {
+        exitFlag = true;  
+    }
+
+    gameMech->incrementScore();   
+  
+    
     
 }
 
 void DrawScreen(void)
-{
-    MacUILib_clearScreen();    
+{       
+    int Board_Width = gameMech->getBoardSizeX(); 
+    int Board_Len = gameMech->getBoardSizeY(); 
+
+        MacUILib_clearScreen();    
+
+
+    for (int y = 0; y < Board_Len; ++y) {   /
+        for (int x = 0; x < Board_Width; ++x) {  
+            if (x == 0 || x == Board_Width - 1 || y == 0 || y == Board_Len - 1) {
+                MacUILib_printf("#"); 
+            } else {
+                MacUILib_printf(" ");
+            }
+        }
+        MacUILib_printf("\n");
+    }
+
+    MacUILib_printf("Score: %d\n", gameMech->getScore());
+    if (gameMech->getExitFlagStatus()) {
+        MacUILib_printf("Exiting the game\n");
+    }
+
+    gameMech->clearInput();
+
+
+
 }
 
 void LoopDelay(void)
 {
-    MacUILib_Delay(DELAY_CONST); // 0.1s delay
+    MacUILib_Delay(DELAY_CONST);
 }
 
 
 void CleanUp(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen(); 
+    delete gameMech;   
 
     MacUILib_uninit();
 }
